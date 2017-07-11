06215a676dd03c4867345015eb89c21638686fc8
Matthew McGriskin <matthewmcgriskin@Matthews-MacBook-Pro.local>
2017-07-11 17:22:50 +0100

    last of code for hangman

--- c5/hangman_game.py
+++ c5/hangman_game.py
@@ -136,7 +136,7 @@ used = []  # letters already guessed
 print("Welcome to Hangman, good luck!")
 
 while wrong < MAX_WRONG and so_far != word:
-    print(HANGMAN[word])
+    print(HANGMAN[wrong])
     print("\nYou've used the following letters:\n", used)
     print("\nSo far, the word is: \n", so_far)
 
@@ -150,4 +150,28 @@ while wrong < MAX_WRONG and so_far != word:
 
     used.append(guess)
 
-    if guess
+    if guess in word:
+        print("\nYes!", guess, "is in the word!")
+
+        # create a new so_far to include guess
+        new = ""
+        for i in range(len(word)):
+            if guess == word[i]:
+                new += guess
+            else:
+                new += so_far[i]
+        so_far = new
+
+    else:
+        print("\nSorry,", guess, "isn't in the word.")
+        wrong += 1
+
+if wrong == MAX_WRONG:
+    print(HANGMAN[wrong])
+    print("\nYou've been hanged!")
+else:
+    print("\nYou guessed it!")
+
+print("\nThe word was", word)
+
+input("\n\nPress the enter key to exit.")
